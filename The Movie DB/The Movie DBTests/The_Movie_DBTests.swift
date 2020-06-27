120cf3558027f1defe32ebea5f7eb4d9d7cceb27
Muhammad Waqas <javamyheart@gmail.com>
2020-06-27 22:46:42 +0500

    -- project created -- structure update -- core data manager created -- pod initiated -- giignore updated

---
+++ The Movie DB/The Movie DBTests/The_Movie_DBTests.swift
@@ -0,0 +1,34 @@
+//
+//  The_Movie_DBTests.swift
+//  The Movie DBTests
+//
+//  Created by Muhammad Jabbar on 6/27/20.
+//  Copyright © 2020 Muhammad Jabbar. All rights reserved.
+//
+
+import XCTest
+@testable import The_Movie_DB
+
+class The_Movie_DBTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
